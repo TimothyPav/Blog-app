5089a92938f3fae2eafed9835f580e8b24bc4550
TimothyPav <1000timka@gmail.com>
2024-03-10 01:44:19 -0600

    added limiter globally in index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware for parsing application/x-www-form-urlencoded
+app.use(limiter);
 
 mongoose.connect('mongodb://localhost:27017/blogDB')
 .then(() => console.log("Connected to MongoDB"))
