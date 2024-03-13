b8b062f6278921016629d7f8268b900496218e6d
TimothyPav <1000timka@gmail.com>
2024-03-12 23:11:56 -0500

    added react to the project and made sure everything is good2go

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// No need to import a CSS file if you don't have any styles yet
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Blog App!</h1>
+    </div>
+  );
+}
+
+export default App;
