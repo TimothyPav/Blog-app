9e2a525272205a7e82f713cfc21e6760652055d1
TimothyPav <1000timka@gmail.com>
2024-03-18 23:56:03 -0500

    so close to finishing navbar

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import {FaTimes} from 'react-icons/fa'
+import {CiMenuFries} from 'react-icons/ci'
 
 export default function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!click);
+    }
+
     const content = <>
-        <div className="">
-            <ul>
-                <Link to="Home">
-                    <li>Home</li>
+        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
+            <ul className="text-center text-xl p-20">
+                <Link spy={true} smooth={true} to="Home">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
                 </Link>
-                <Link to="Profile">
-                    <li>Profile</li>
+                <Link spy={true} smooth={true} to="Profile">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Profile</li>
                 </Link>
-                <Link to="SignIn">
-                    <li>Sign In</li>
+                <Link spy={true} smooth={true} to="SignIn">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Sign In</li>
                 </Link>
             </ul>
         </div>
@@ -25,22 +32,30 @@ export default function Navbar() {
                     <div className="flex items-center flex-1">
                         <span className="text-3xl font-bold">Logo</span>
                     </div>
-                    <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal">
+                    <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal">
                         <div className="flex-10">
                         <ul className="flex gap-8 mr-16 text-[18px]">
-                            <Link to="Home">
-                                <li>Home</li>
+                            <Link spy={true} smooth={true} to="Home">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer" >Home</li>
                             </Link>
-                            <Link to="Profile">
-                                <li>Profile</li>
+                            <Link spy={true} smooth={true} to="Profile">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer" >Profile</li>
                             </Link>
-                            <Link to="SignIn">
-                                <li>Sign In</li>
+                            <Link spy={true} smooth={true} to="SignIn">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer" >Sign In</li>
                             </Link>
                         </ul>
                         </div>
                     </div>
+                    <div>
+                        {click && content}
+                    </div>
+
+                    <button className="block sm:hidden transition" onClick={handleClick}>
+                        {click ? <FaTimes/> : <CiMenuFries/>}
+                    </button>
                 </div>
+                
             </nav>
         </>
     );
